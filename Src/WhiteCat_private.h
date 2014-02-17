2d8d6d33815aa1f69e130c62226965cb5f232ee8
AntonLanghoff <antonlanghoff@hotmail.com>
2014-02-17 17:43:28 +0100

    add source code to the directory

---
+++ Src/WhiteCat_private.h
@@ -0,0 +1,21 @@
+
+#ifndef WHITECAT_PRIVATE_H
+#define WHITECAT_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.8.15.1"
+#define VER_MAJOR	0
+#define VER_MINOR	8
+#define VER_RELEASE	15
+#define VER_BUILD	1
+#define COMPANY_NAME	"www.le-chat-noir-numerique.fr"
+#define FILE_VERSION	"béta 0.8.15.1"
+#define FILE_DESCRIPTION	"White Cat: a dmx lighting board for theatre dance and music"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"(c)Christoph Guillermet"
+#define LEGAL_TRADEMARKS	"(r)White Cat is  INPI's registered trademark"
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"White Cat Lighting Board, Spring version"
+#define PRODUCT_VERSION	"17_02_2014"
+
+#endif /*WHITECAT_PRIVATE_H*/
---
+++ Src/arduino_device_core.cpp
@@ -0,0 +1,72 @@
+/*-------------------------------------------------------------------------------------------------------------
+                                 |
+          CWWWWWWWW              | Copyright (C) 2009-2013  Christoph Guillermet
+       WWWWWWWWWWWWWWW           | 
+     WWWWWWWWWWWWWWWWWWW         | This file is part of White Cat.
+    WWWWWWWWWWWWWWWWWCWWWW       | 
+   WWWWWWWWWWWWWWWWW tWWWWW      | White Cat is free software: you can redistribute it and/or modify
+  WWWW   WWWWWWWWWW  tWWWWWW     | it under the terms of the GNU General Public License as published by
+ WWWWWt              tWWWWWWa    | the Free Software Foundation, either version 3 of the License, or
+ WWWWWW               WWWWWWW    | (at your option) any later version.
+WWWWWWWW              WWWWWWW    | 
+WWWWWWWW               WWWWWWW   | White Cat is distributed in the hope that it will be useful,
+WWWWWWW               WWWWWWWW   | but WITHOUT ANY WARRANTY; without even the implied warranty of
+WWWWWWW      CWWW    W WWWWWWW   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+WWWWWWW            aW  WWWWWWW   | GNU General Public License for more details.
+WWWWWWWW           C  WWWWWWWW   | 
+ WWWWWWWW            CWWWWWWW    | You should have received a copy of the GNU General Public License
+ WWWWWWWWW          WWWWWWWWW    | along with White Cat.  If not, see <http://www.gnu.org/licenses/>. 
+  WWWWWWWWWWC    CWWWWWWWWWW     |   
+   WWWWWWWWWWWWWWWWWWWWWWWW      | 
+    WWWWWWWWWWWWWWWWWWWWWW       |    
+      WWWWWWWWWWWWWWWWWWa        |     
+        WWWWWWWWWWWWWWW          |     
+           WWWWWWWWt             |
+                                 |
+---------------------------------------------------------------------------------------------------------------*/
+
+/**
+
+ \file arduino_device_core.cpp
+ \brief {description courte} 
+ \author Christoph Guillermet
+ \version {numero de version du fichier}
+ \date {date description}
+ 
+ White Cat {- catégorie} {- sous catégorie {- sous catégorie}}
+ Description détaillée
+ 
+ **/
+ 
+ #include "serial.cpp"
+CSerial serial0;
+
+int arduino_init(int device)
+{
+if(device==0)
+{
+if(serial0.Open(arduino_com0,arduino_baud_rate0))
+{
+sprintf(string_Arduino_status,">>ARDUINO : Open Arduino on PORT %d rate: %d >> OK !",arduino_com0,arduino_baud_rate0);  
+arduino_device_0_is_ignited=1;
+//rest(1000); //temps d'initialisation avant communication ( sinon pense recuperer script)
+}
+else 
+{
+sprintf(string_Arduino_status,">>ARDUINO : Open Arduino on PORT %d >> FAILED !",arduino_com0);
+arduino_device_0_is_ignited=0;  
+}
+}
+return(0);              
+}
+
+int arduino_close(int device)
+{
+if(device==0)
+{
+ serial0.Close();
+ sprintf(string_Arduino_status,">>ARDUINO : Closed Arduino on PORT %d >> OK !",arduino_com0); 
+}
+ 
+return(0);   
+}
