c268db799e89f0dc3a2b5f8783cbf8ce15902697
Rui Serge <ruiserge@club-internet.fr>
2014-06-29 13:18:08 +0200

    CppCheck - Defensive programming The variable 'banger_is' is used as an array index before it is checked that is within limits. This can mean that the array might be accessed out of bounds.

--- Src/banger_core_8.cpp
+++ Src/banger_core_8.cpp
@@ -4692,42 +4692,47 @@ if(do_loop_banger[banger_is]==1 && time_loop_banger[banger_is]>0.0 && ticker_loo
 
 int do_bang(int banger_is)
 {
-if( bang_is_sended[banger_is]==0 && banger_is<127)
-{
-end_time_for_banger[banger_is]=0;//reinit pour pierre groupe laps
-//calcul bang time de fin
-for (int y=0;y<6;y++)
-{
-if(bangers_delay[banger_is][y]> end_time_for_banger[banger_is])
-{
-end_time_for_banger[banger_is]= bangers_delay[banger_is][y];
-}
-}
-if(end_time_for_banger[banger_is]<default_time_of_the_bang)
-{end_time_for_banger[banger_is]=default_time_of_the_bang;}
-///////////////////////
+//sab 29/06/2014 CppCheck if( bang_is_sended[banger_is]==0 && banger_is<127)
+    if  (banger_is>=core_user_define_nb_bangers) return 0; //Test de dépassement de capacité avant utilistion des tableaux
+
+    if( bang_is_sended[banger_is]==0)
+    {
+        end_time_for_banger[banger_is]=0;//reinit pour pierre groupe laps
+		//calcul bang time de fin
+        for (int y=0; y<6; y++)
+        {
+            if(bangers_delay[banger_is][y]> end_time_for_banger[banger_is])
+            {
+                end_time_for_banger[banger_is]= bangers_delay[banger_is][y];
+            }
+        }
+        if(end_time_for_banger[banger_is]<default_time_of_the_bang)
+        {
+            end_time_for_banger[banger_is]=default_time_of_the_bang;
+        }
+		///////////////////////
 
 
-for (int y=0;y<6;y++)
-{
-if(bangers_type[banger_is][y]!=0)
-{
+        for (int y=0; y<6; y++)
+        {
+            if(bangers_type[banger_is][y]!=0)
+            {
 
-if(actual_time>start_time_for_banger[banger_is]+ (bangers_delay[banger_is][y]*BPS_RATE) && event_sended[banger_is][y]==0)
-{
-    Bang_event(banger_is, y);
-    event_sended[banger_is][y]=1;
-}
-}
-}
+                if(actual_time>start_time_for_banger[banger_is]+ (bangers_delay[banger_is][y]*BPS_RATE) && event_sended[banger_is][y]==0)
+                {
+                    Bang_event(banger_is, y);
+                    event_sended[banger_is][y]=1;
+                }
+            }
+        }
 
-if(actual_time>(start_time_for_banger[banger_is]+ (end_time_for_banger[banger_is]*BPS_RATE)))//rajout de +50 pour visualisation send dernier event
-{
-bang_is_sended[banger_is]=1;
-}
+        if(actual_time>(start_time_for_banger[banger_is]+ (end_time_for_banger[banger_is]*BPS_RATE)))//rajout de +50 pour visualisation send dernier event
+        {
+            bang_is_sended[banger_is]=1;
+        }
 
-}
-return(0);
+    }
+    return(0);
 }
 
 
